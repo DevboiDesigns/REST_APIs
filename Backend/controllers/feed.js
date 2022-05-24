7bab3639b00f914dff15024920cfb15d62f62d8e
Christopher Hicks <visrule@protonmail.com>
2022-05-24 16:34:20 -0500

    post post method

--- Backend/controllers/feed.js
+++ Backend/controllers/feed.js
@@ -1,16 +1,33 @@
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts: [{ title: 'First Post', content: 'This is the first post!' }]
-    })
-}
+  res.status(200).json({
+    posts: [
+      {
+        _id: "1",
+        title: "First Post",
+        content: "This is the first post!",
+        imageUrl: "images/abstract.jpg",
+        creator: {
+          name: "Maximillian",
+        },
+        createdAt: new Date(),
+      },
+    ],
+  });
+};
 
 exports.postAPost = (req, res, next) => {
-    const title = req.body.title
-    const content = req.body.content
-    console.log(title, content)
-    // create post in db
-    res.status(201).json({
-        message: 'Post created succesfully',
-        post: {id: new Date().toISOString(), title: title, content: content}
-    })
-}
+  const title = req.body.title;
+  const content = req.body.content;
+  console.log(title, content);
+  // create post in db
+  res.status(201).json({
+    message: "Post created succesfully",
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: { name: "Josh" },
+      createdAt: new Date(),
+    },
+  });
+};
